44798947396cf50ca69282e64b1038ee589f25ac
Rodrigo Silva <rodrigosilvaro46@gmail.com>
2025-02-14 18:04:27 -0300

    implementa request e response

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteResponse.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteResponse.java
@@ -1,5 +1,10 @@
 package br.com.petz.cliente_pet.cliente.application.api;
 
-public class ClienteResponse {
+import java.util.UUID;
 
+import lombok.Value;
+
+@Value
+public class ClienteResponse {
+	private UUID idCliente;
 }
