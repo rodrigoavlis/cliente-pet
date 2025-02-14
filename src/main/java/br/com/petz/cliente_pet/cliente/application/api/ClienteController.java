6299f05441ea43be884ab88c73a1eba11b2520f9
Rodrigo Silva <rodrigosilvaro46@gmail.com>
2025-02-14 17:58:30 -0300

    implementa controlle e fluxo para service

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteController.java
@@ -2,17 +2,23 @@ package br.com.petz.cliente_pet.cliente.application.api;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.petz.cliente_pet.cliente.application.sevice.ClienteService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @RestController
 @Log4j2
+@RequiredArgsConstructor
+
 public class ClienteController implements ClienteAPI {
+	private final ClienteService clienteService;
 
 	@Override
 	public ClienteResponse postCliente(clienteRequest clienteRequest) {
 		log.info("[inicia] ClienteController - postCliente");
+		ClienteResponse clienteCriado = clienteService.criaCliente(clienteRequest);
 		log.info("[finaliza] ClienteController - postCliente");
-		return null;
+		return clienteCriado;
 	}
 
 }
---
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/sevice/ClienteService.java
@@ -0,0 +1,10 @@
+package br.com.petz.cliente_pet.cliente.application.sevice;
+
+import br.com.petz.cliente_pet.cliente.application.api.ClienteResponse;
+import br.com.petz.cliente_pet.cliente.application.api.clienteRequest;
+
+public interface ClienteService {
+
+	ClienteResponse criaCliente(clienteRequest clienteRequest);
+
+}
