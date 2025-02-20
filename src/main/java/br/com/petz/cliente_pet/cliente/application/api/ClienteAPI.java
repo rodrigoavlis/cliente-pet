cf41b6a2ff03207cefae01b10f75f098ce6c9b63
Rodrigo Silva <rodrigosilvaro46@gmail.com>
2025-02-20 15:25:22 -0300

    implementa fluxo api, teste ok

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteAPI.java
@@ -1,6 +1,9 @@
 package br.com.petz.cliente_pet.cliente.application.api;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,4 +19,8 @@ public interface ClienteAPI {
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
 	ClienteResponse postCliente(@Valid @RequestBody ClienteRequest clienteRequest);
+	
+	@GetMapping
+	@ResponseStatus(code = HttpStatus.OK)
+	List<ClienteListResponse> getTodosCliente();
 }
