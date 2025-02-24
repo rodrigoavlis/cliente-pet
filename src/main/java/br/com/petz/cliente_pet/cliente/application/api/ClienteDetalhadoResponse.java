c8b7ef78f232f919bc390a37204ce58e9d6ed39c
Rodrigo Silva <rodrigosilvaro46@gmail.com>
2025-02-24 14:44:52 -0300

    implementa API de Get Cliente atraves do ID

---
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteDetalhadoResponse.java
@@ -0,0 +1,21 @@
+package br.com.petz.cliente_pet.cliente.application.api;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import br.com.petz.cliente_pet.cliente.domain.Sexo;
+import lombok.Value;
+
+@Value
+public class ClienteDetalhadoResponse {
+		private UUID idCliente;
+		private String nomeCompleto;
+		private String cpf;
+		private String email;
+		private Boolean aceitaTermos;
+		private Sexo sexo;
+		private String celular;
+		private String telefone;
+		private LocalDateTime dataHoraDoCadastro;
+
+}
