50280ff1271b5299a96371bf4f6ef97cf03d6fda
Rodrigo Silva <rodrigosilvaro46@gmail.com>
2025-03-03 22:17:11 -0300

    implementa fluxo da API

--- src/main/java/br/com/petz/cliente_pet/pet/domain/Pet.java
+++ src/main/java/br/com/petz/cliente_pet/pet/domain/Pet.java
@@ -4,11 +4,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import org.hibernate.validator.constraints.br.CPF;
-
-import br.com.petz.cliente_pet.cliente.application.api.ClienteAlteracaoRequest;
-import br.com.petz.cliente_pet.cliente.application.api.ClienteRequest;
-import br.com.petz.cliente_pet.cliente.domain.Sexo;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -16,7 +11,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
